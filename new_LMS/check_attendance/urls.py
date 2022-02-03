24f11627c32813d949581131fed7dd955bd1e757
SEOK <seokjam@gmail.com>
2022-02-03 15:09:17 +0900

    client, manager html 생성 url, view 수정

--- new_LMS/check_attendance/urls.py
+++ new_LMS/check_attendance/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('', views.manager_view),
+    path('', views.client_view),
 ]
--- new_LMS/check_attendance/views.py
+++ new_LMS/check_attendance/views.py
@@ -8,3 +8,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def client_view(request):
+    return render(request, 'client.html')
+
+def manager_view(request):
+    return render(request, 'manager.html')
